c39ac916c96d0190ee0338e134f1da499bcd0865
mrizkystr <muhamadrizkysatria23@gmail.com>
2023-11-16 20:50:37 +0700

    first commit

---
+++ ecommerce/pages/contact.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Ini halaman contanct</h1>
+</template>
---
+++ ecommerce/pages/product/[id].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { products } from "../../composables/constants/product";
+import { Products } from "../../types/products";
+const route = useRoute();
+const id =
+  typeof route.params.id === "string" ? parseInt(route.params.id) : null;
+const product = ref(products.find((item: Products) => item.id === id));
+</script>
+<template>
+  <DetailProduct :product="product" />
+</template>
